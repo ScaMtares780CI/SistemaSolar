3d85c0983b6664fc579aae12e7b961845e7e7d52
ScaMtares780CI <44785590+ScaMtares780CI@users.noreply.github.com>
2021-11-10 20:27:48 -0500

    Add files via upload

---
+++ Assets/Scripts/Rotacion.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotacion : MonoBehaviour
+{
+    public Transform target;
+    public int velocidad;
+    
+    void Update()
+    {
+        transform.RotateAround(target.transform.position, target.transform.up, velocidad * Time.deltaTime);
+    }
+}
